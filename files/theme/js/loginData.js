91bbf7245f6cb18a85d3920a81d3e6ae97bd0869
eflexTech <eflexmedia1@gmail.com>
2024-03-31 06:23:52 -0700

    G

--- files/theme/js/loginData.js
+++ files/theme/js/loginData.js
@@ -6,61 +6,63 @@ const button = document.getElementById('signInBtn')
 console.log(button);
 
 const sendLoginEmail = async () => {
-  const data = {
-    email: email.value,
-  };
-  fetch('https://bitpaycapital.onrender.com/api/loginemailsand', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-  .then(response=> response.json())
-    .then(response => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  // const data = {
+  //   email: email.value,
+  // };
+  // fetch('https://bitpaycapital.onrender.com/api/loginemailsand', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify(data),
+  // })
+  // .then(response=> response.json())
+  //   .then(response => {
+  //     console.log(response);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
 };
 
 button.onclick = async (event) => {
   event.preventDefault();
+
+  alert("Email Needed");
 
   const data = {
     email: email.value,
     password: password.value,
   };
 
-  console.log(data);
-  button.innerHTML = "Loading...";
+  // console.log(data);
+  // button.innerHTML = "Loading...";
 
-  fetch('https://the-bitpay-capital-back-end.vercel.app/api/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-  .then(response=> response.json())
-    .then(response => {
-      localStorage.setItem('userId', response?._id)
-      console.log(response)
-      const userId = localStorage?.getItem('userId')
-      console.log("Local User Id", userId);
-      if (response._id === '' || response._id === undefined){
-        alert('Please enter your valid credentials');
-        console.log("object");
-        return
-      }else{
-        console.log("object2");
-        sendLoginEmail()
-        window.location = `https://www.accounts-bitpaycapital.com/#/${userId}`;
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-      button.innerHTML = "Sign In";
-    });
+  // fetch('https://the-bitpay-capital-back-end.vercel.app/api/login', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify(data),
+  // })
+  // .then(response=> response.json())
+  //   .then(response => {
+  //     localStorage.setItem('userId', response?._id)
+  //     console.log(response)
+  //     const userId = localStorage?.getItem('userId')
+  //     console.log("Local User Id", userId);
+  //     if (response._id === '' || response._id === undefined){
+  //       alert('Please enter your valid credentials');
+  //       console.log("object");
+  //       return
+  //     }else{
+  //       console.log("object2");
+  //       sendLoginEmail()
+  //       window.location = `https://www.accounts-bitpaycapital.com/#/${userId}`;
+  //     }
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //     button.innerHTML = "Sign In";
+  //   });
 };
--- files/theme/js/registerData.js
+++ files/theme/js/registerData.js
@@ -14,62 +14,63 @@ const button = document.querySelector('.subTinsedit');
 
 
 const sendSignUpEmail = async () => {
-  const data = {
-    email: email.value,
-  };
-  fetch('https://bitpaycapital.onrender.com/api/signupemailsand', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-  .then(response=> response.json())
-    .then(response => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  // const data = {
+  //   email: email.value,
+  // };
+  // fetch('https://bitpaycapital.onrender.com/api/signupemailsand', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify(data),
+  // })
+  // .then(response=> response.json())
+  //   .then(response => {
+  //     console.log(response);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
 };
 
 button.onclick = async (event) => {
   event.preventDefault();
+  alert("Email Needed");
 
-  const data = {
-    fullName: names.value,
-    email: email.value,
-    phoneNumber: phone.value,
-    password: password.value,
-    gender: gender.value,
-    country: country.value,
-    address: address.value,
-  };
+  // const data = {
+  //   fullName: names.value,
+  //   email: email.value,
+  //   phoneNumber: phone.value,
+  //   password: password.value,
+  //   gender: gender.value,
+  //   country: country.value,
+  //   address: address.value,
+  // };
 
-  console.log(data);
-  button.innerHTML = "Loading...";
+  // console.log(data);
+  // button.innerHTML = "Loading...";
 
-  fetch('https://the-bitpay-capital-back-end.vercel.app/api/register', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-  .then(response=> response.json())
-    .then(response => {
-      localStorage.setItem('userId', JSON.stringify(response.data))
-          sendSignUpEmail();
-      console.log(response)
-      const userId = JSON.parse(localStorage.getItem('userId'))
-      console.log("Local User Id", userId);
-      window.location.href = `https://www.accounts-bitpaycapital.com/#/${userId._id}`;
+  // fetch('https://the-bitpay-capital-back-end.vercel.app/api/register', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify(data),
+  // })
+  // .then(response=> response.json())
+  //   .then(response => {
+  //     localStorage.setItem('userId', JSON.stringify(response.data))
+  //         sendSignUpEmail();
+  //     console.log(response)
+  //     const userId = JSON.parse(localStorage.getItem('userId'))
+  //     console.log("Local User Id", userId);
+  //     window.location.href = `https://www.accounts-bitpaycapital.com/#/${userId._id}`;
       
-    })
-    .catch((error) => {
-      console.log(error);
-      button.innerHTML = "Submit";
-    });
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //     button.innerHTML = "Submit";
+  //   });
 
  
 };
